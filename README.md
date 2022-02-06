199414fe802a48f8293fc4ee271c90d270bc66ee
bgtti <bru.mgtti@gmail.com>
2022-02-06 12:35:01 +0100

    base functionality

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # library
+
+Live preview: https://bgtti.github.io/library/
+
+This 'Book Library' enables the user to keep track of books the user has read, is reading, or wants to read.
+The user is able to set up a book table, add new items, edit them and delete them.
+
+The purpose of the project was to use contructor functions, built using html, css, and vanilla JavaScript.
+
+The Book Library is part of the Odin Project assignment. For more information about the guidelines, please refer to: https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/library
--- index.js
+++ index.js
@@ -1,66 +1,285 @@
-// Open/Close Modal
-const closeModal = document.querySelector('#closeModal');
+//OPEN AND CLOSE MODALS
+function closeModalF(modalToClose) {
+	modalToClose.classList.add('hideModal')
+}
+function openModalF(modalToOpen) {
+	modalToOpen.classList.remove('hideModal')
+}
+
+// Open/Close 'Add new book' Modal
+const closeModal = document.querySelector('#closeModal')
 const modalContainer = document.querySelector('#modalContainer')
 const addBookBtn = document.querySelector('#addBookBtn')
 
-function closeModalF (){
- modalContainer.classList.add('hideModal');
+closeModal.addEventListener(
+	'click',
+	() => {
+		closeModalF(modalContainer)
+	},
+	false
+)
+addBookBtn.addEventListener(
+	'click',
+	() => {
+		openModalF(modalContainer)
+	},
+	false
+)
+//Closing 'delete book' modal
+//(the opening of the modal by clicking the trash icon is done from the showTable function)
+const closeDeleteModal = document.querySelector('#closeDeleteModal')
+const deleteModalContainer = document.querySelector('#deleteModalContainer')
+const keepBookBtn = document.querySelector('#keepBookBtn')
+
+let bookToDelete = undefined //when the trash can clicked, index saved to this variable
+
+closeDeleteModal.addEventListener(
+	'click',
+	() => {
+		closeModalF(deleteModalContainer)
+		bookToDelete = undefined
+	},
+	false
+)
+keepBookBtn.addEventListener(
+	'click',
+	() => {
+		closeModalF(deleteModalContainer)
+		bookToDelete = undefined
+	},
+	false
+)
+
+//SAMPLE BOOKS
+
+const sampleBook1 = {
+	title: 'The Black Swan',
+	author: 'N. N. Taleb',
+	pages: '444',
+	status: 'Read',
+	rating: '5',
+	comments:
+		'Book about the impact of rare and unpredictable extreme events, and the human tendency to find simplistic explanations for these in retrospect.',
 }
-function openModalF (){
- modalContainer.classList.remove('hideModal');
+const sampleBook2 = {
+	title: 'Lord of the Rings',
+	author: 'J. R. R. Tolkien',
+	pages: '1248',
+	status: 'Read',
+	rating: '5',
+	comments: 'A massive but great book!',
 }
-closeModal.addEventListener('click', ()=>{
-closeModalF();
-}, false);
-
-addBookBtn.addEventListener('click', ()=>{
-openModalF();
-}, false);
-
-let myLibrary = [];
-function Book(title, author,pages,status,rating){
- this.title = title;
- this.author = author;
- this.pages = pages;
- this.status = status;
- this.rating = rating;
- this.comments = comments;
+const sampleBook3 = {
+	title: 'One Hundred Years of Solitude',
+	author: 'Gabriel García Márquez',
+	pages: '417',
+	status: 'Read',
+	rating: '4',
+	comments: 'A massive but great book!',
 }
 
-let authorInput = document.querySelector('#author');
-let titleInput = document.querySelector('#title');
-let pagesInput = document.querySelector('#pages');
-let statusInput = document.querySelector('#status');
-let ratingInput = document.querySelector('#rating');
-let commentsInput = document.querySelector('#comments');
+//BOOK LIBRARY: the array where book objects will be saved to
+let myLibrary = [sampleBook1, sampleBook2, sampleBook3]
 
-function resetInputValues(){
-authorInput.value = "";
-titleInput.value = "";
-pagesInput.value = "";
-ratingInput.value = "";
-commentsInput.value = "";
-
-
-
-}
-function addBookToLibrary(){
- myLibrary.push(new Book(authorInput.value, titleInput.value, pagesInput.value, statusInput.value, ratingInput.value, commentsInput.value ))
- myLibrary.push(authorInput.value);
- console.log(myLibrary);
-resetInputValues();
- 
+//CONSTRUCTOR of the book object:
+function Book(title, author, pages, status, rating) {
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.status = status
+	this.rating = rating
+	this.comments = comments
 }
 
-function addNewBookToTable (){
- 
+//ADD NEW BOOK MODAL
+//Selection of inputs from 'Add new book' Modal:
+const authorInput = document.querySelector('#author')
+const titleInput = document.querySelector('#title')
+const pagesInput = document.querySelector('#pages')
+const statusInput = document.querySelector('#status')
+const ratingInput = document.querySelector('#rating')
+const theStars = document.querySelectorAll('.theStars')
+const commentsInput = document.querySelector('#comments')
+
+//The star rating: sets a rating value to ratingInput (in 'Add new book' Modal)
+for (let star of theStars) {
+	star.addEventListener(
+		'click',
+		() => {
+			ratingInput.value = star.dataset.rating
+
+			theStars.forEach((star) => {
+				if (star.dataset.rating <= ratingInput.value) {
+					star.classList.add('selectedStar')
+					star.children[0].removeAttribute('name', 'star-outline')
+					star.children[0].setAttribute('name', 'star')
+				} else {
+					star.classList.remove('selectedStar')
+					star.children[0].removeAttribute('name', 'star')
+					star.children[0].setAttribute('name', 'star-outline')
+				}
+			})
+		},
+		false
+	)
+}
+//Reset button in 'Add new book' modal: resetting the stars
+const resetBtn = document.querySelector('#resetBtn')
+
+function resetStars() {
+	for (let star of theStars) {
+		star.classList.remove('selectedStar')
+		star.children[0].removeAttribute('name', 'star')
+		star.children[0].setAttribute('name', 'star-outline')
+	}
 }
 
-const saveBookBtn = document.querySelector('#saveBookBtn');
-saveBookBtn.addEventListener('click', ()=>{
-addBookToLibrary();
-closeModalF();
-}, false)
+resetBtn.addEventListener(
+	'click',
+	() => {
+		resetStars()
+	},
+	false
+)
 
+//resetting input in Modal (after clicking "save book"), which is called by the addBookToLibrary function
+function resetInputValues() {
+	authorInput.value = ''
+	titleInput.value = ''
+	pagesInput.value = ''
+	ratingInput.value = ''
+	commentsInput.value = ''
+	resetStars()
+}
 
+//function that creates a new book object:
+function addBookToLibrary() {
+	myLibrary.push(
+		new Book(
+			titleInput.value,
+			authorInput.value,
+			pagesInput.value,
+			statusInput.value,
+			ratingInput.value,
+			commentsInput.value
+		)
+	)
+	showTable()
+	resetInputValues()
+}
 
+//BOOK LIST
+//selectors:
+let trashIcons = undefined
+let editIcons = undefined
+
+let bookTable = document.querySelector('#bookTable')
+
+//function that adds newly created book to 'Book List': to be viewed by user
+function showTable() {
+	while (bookTable.children.length > 1) {
+		bookTable.removeChild(bookTable.lastChild)
+	}
+	if (myLibrary.length === 0) {
+		let newEmptyRow = document.createElement('tr')
+		newEmptyRow.classList.add('book-row')
+		newEmptyRow.textContent = "You don't have books"
+		bookTable.append(newEmptyRow)
+	} else {
+		for (let book of myLibrary) {
+			let newRow = document.createElement('tr')
+			newRow.classList.add('book-row')
+			bookTable.append(newRow)
+
+			let taps = [book.title, book.author, book.pages, book.status]
+			let nrOfStars = parseInt([book.rating])
+			if (!nrOfStars) {
+				nrOfStars = 0
+			}
+
+			for (let el in taps) {
+				let newCell = document.createElement('td')
+				newCell.textContent = taps[el]
+				newRow.append(newCell)
+			}
+
+			let newCellRating = document.createElement('td')
+			for (let i = 1; i <= nrOfStars; i++) {
+				let fullStar = document.createElement('ion-icon')
+				fullStar.setAttribute('name', 'star')
+				fullStar.classList.add('ministarIcon')
+				newCellRating.append(fullStar)
+			}
+			for (let j = 1; j <= 5 - nrOfStars; j++) {
+				let outlineStar = document.createElement('ion-icon')
+				outlineStar.setAttribute('name', 'star-outline')
+				outlineStar.classList.add('ministarIcon')
+				newCellRating.append(outlineStar)
+			}
+			newRow.append(newCellRating)
+
+			let actionCell = document.createElement('td')
+			let newEditIcon = document.createElement('ion-icon')
+			newEditIcon.setAttribute('name', 'create-outline')
+			newEditIcon.classList.add('editIcon')
+
+			let newTrashIcon = document.createElement('ion-icon')
+			newTrashIcon.setAttribute('name', 'trash-outline')
+			newTrashIcon.classList.add('trashIcon')
+
+			actionCell.append(newEditIcon)
+			actionCell.append(newTrashIcon)
+			newRow.append(actionCell)
+		}
+		trashIcons = document.querySelectorAll('.trashIcon')
+		editIcons = document.querySelectorAll('.editIcon')
+
+		console.log(trashIcons) // *****************************************************REMOVE
+
+		trashIcons.forEach((e, i) => {
+			e.addEventListener(
+				'click',
+				() => {
+					openModalF(deleteModalContainer)
+					bookToDelete = i
+					console.log(bookToDelete) //// **************************************REMOVE
+				},
+				false
+			)
+		})
+	}
+}
+showTable()
+
+//SAVE NEW BOOK
+//Event listener to the 'save book' button in 'Add new book' Modal:
+//it will call the function to create a new book object (addBookToLibrary) and close the modal.
+const saveBookBtn = document.querySelector('#saveBookBtn')
+
+saveBookBtn.addEventListener(
+	'click',
+	() => {
+		addBookToLibrary()
+		closeModalF(modalContainer)
+	},
+	false
+)
+
+//DELETE BOOK MODAL: the delete button
+//deleting book from list
+const deleteBtn = document.querySelector('#deleteBtn')
+
+function deleteBook() {
+	myLibrary.splice(bookToDelete, 1)
+	bookToDelete = undefined
+	showTable()
+	closeModalF(deleteModalContainer)
+}
+
+deleteBtn.addEventListener(
+	'click',
+	() => {
+		deleteBook()
+	},
+	false
+)
